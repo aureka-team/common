aa23f144d2726211a37c8b3c6c060409f8906da4
Bas <bastiansg.bas@gmail.com>
2024-05-30 15:06:55 -0300

    RedisCache improvements

--- common/cache/redis_cache.py
+++ common/cache/redis_cache.py
@@ -22,14 +22,21 @@ class RedisCache:
             db=redis_db,
         )
 
-    def save(self, obj: Any, cache_key: str) -> None:
-        if self.redis_client.exists(cache_key):
-            logger.warning(f"cache_key: {cache_key} already exists")
-            return
+    def save(self, obj: Any, cache_key: str, overwrite: bool = False) -> None:
+        if not overwrite:
+            if self.redis_client.exists(cache_key):
+                logger.warning(f"cache_key: {cache_key} already exists")
+                return
 
         logger.debug(f"saving in cache: {cache_key}")
-        data = dill.dumps(obj)
-        self.redis_client.mset({cache_key: data})
+        self.redis_client.set(
+            name=cache_key,
+            value=dill.dumps(obj),
+        )
+
+    def msave(self, objs: dict[str, Any]) -> None:
+        logger.debug(f"saving in cache: {objs}")
+        self.redis_client.mset({k: dill.dumps(v) for k, v in objs.items()})
 
     def load(self, cache_key: str) -> Optional[Any]:
         if not self.redis_client.exists(cache_key):
@@ -40,3 +47,8 @@ class RedisCache:
         obj = dill.loads(obj)
 
         return obj
+
+    def mload(self, cache_keys: list[str]) -> list[Any]:
+        logger.debug(f"loading from cache: {cache_keys}")
+        objs = self.redis_client.mget(cache_keys)
+        return [dill.loads(obj) if obj is not None else obj for obj in objs]
